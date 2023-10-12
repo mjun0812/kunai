894ed769510bcfe32f8eee144acf0f74c6036bad
Junya Morioka <mjun@mjunya.com>
2023-10-12 22:19:44 +0900

    update

--- kunai/torch_utils/__init__.py
+++ kunai/torch_utils/__init__.py
@@ -1,7 +1,7 @@
 """kunai.torch"""
 
 from .cuda import cuda_info, set_device, time_synchronized
-from .model_util import save_model, save_model_info, check_model_parallel
+from .model_util import save_model, save_model_info, check_model_parallel, prety_print_model_param, calc_params
 from .seed import fix_seed, worker_init_fn
 
 __all__ = [
@@ -13,4 +13,6 @@ __all__ = [
     "fix_seed",
     "worker_init_fn",
     "check_model_parallel",
+    "prety_print_model_param",
+    "calc_params",
 ]
